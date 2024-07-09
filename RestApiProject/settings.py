faff867e6ad71908104b36328e589c15189dba7c
oleetech <rebelsoft111122@gmail.com>
2024-07-09 11:24:49 +0600

    media configure

--- RestApiProject/settings.py
+++ RestApiProject/settings.py
@@ -122,6 +122,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Define your STATIC_ROOT
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
